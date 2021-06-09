32c9e751a400644807070cf13475b6d914cdb8d0
zalexvic <azaburdyaev1@gmail.com>
2021-06-10 00:36:59 +0300

    changed templates

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -21,6 +21,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('chat/', include('chat.urls')),
+    path('', include('chat.urls')),
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
